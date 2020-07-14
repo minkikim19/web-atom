188bd4e0797995d9939c9b08c66ee342dba68c64
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-14 22:37:46 +0900

    Add files via upload
    up

---
+++ syntax/string.js
@@ -0,0 +1,3 @@
+console.log('1', '1');
+
+console.log('iojd slijfid jlds jiofr;edsjf i0jsid jfilasj diolfjeis jdkuhfkjsdhuik hfskudh fkusdh kufhsweuik dhfkuseh dukfh sawkudhfkshweda kufhasewk dhfkashd kfdh nzsd ku fhjxc htfkjhvcxhjfh.'.length);
