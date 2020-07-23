09461f3ec97301b5d7941fdd3363da3672732d45
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-23 15:38:51 +0900

    Add files via upload
    up

--- nodeJs/main.js
+++ nodeJs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -62,7 +63,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
 
         var list = templateList(filelist);
-        var template = templateHTML(title, list, `<form action="http://localhost:3000/process_create" method="post">
+        var template = templateHTML(title, list, `<form action="http://localhost:3000/create_process" method="post">
         <p><input type="text" name="title"  placeholder="title"></p>
         <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -76,7 +77,26 @@ var app = http.createServer(function(request,response){
         response.end(template);
       });
     }
-    else {
+    else if(pathname === '/create_process')
+    {
+      var body='';
+      request.on('data', function(data)
+    {
+      body = body + data;
+    });
+      request.on('end', function()
+    {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      console.log(post.title);
+      console.log(post.description);
+    });
+      response.writeHead(200);
+      response.end('success');
+    }
+    else
+    {
       response.writeHead(404);
       response.end('Not found');
     }
