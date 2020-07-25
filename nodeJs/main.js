42aedb3822d54b26bd407f42f21f00973db20ba2
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-25 10:15:55 +0900

    Add files via upload
    up

--- nodeJs/main.js
+++ nodeJs/main.js
@@ -71,7 +71,7 @@ var app = http.createServer(function(request,response){
         var list = templateList(filelist);
         var template = templateHTML(title, list,
         `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
         <p>
           <input type="text" name="title"  placeholder="title"></p>
         <p>
@@ -106,6 +106,58 @@ var app = http.createServer(function(request,response){
       })
     });
 
+    }
+    else if(pathname === '/update')
+    {
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p>
+              <input type="text" name="title"  placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
+    else if(pathname === '/update_process')
+    {
+        var body='';
+        request.on('data', function(data)
+      {
+        body = body + data;
+      });
+        request.on('end', function()
+      {
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error)
+      {
+        console.log(post);
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err)
+        {
+          response.writeHead(302, {Location: `/?id=${title}`}); //한국어로 생성하면 오류 발생
+          response.end();
+          })
+      })
+
+      });
     }
     else
     {
