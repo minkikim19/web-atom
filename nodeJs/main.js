064ea98b99678d4afd4700355010963ed070cce4
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-27 06:13:43 +0900

    Add files via upload
    삭제버튼 구현, 글 삭제 기능 구현

--- nodeJs/main.js
+++ nodeJs/main.js
@@ -55,7 +55,13 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `<a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post" onsubmit="?">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>
+                `
             );
             response.writeHead(200);
             response.end(template);
@@ -157,6 +163,24 @@ var app = http.createServer(function(request,response){
           })
       })
 
+      });
+    }
+    else if(pathname === '/delete_process')
+    {
+        var body='';
+        request.on('data', function(data)
+      {
+        body = body + data;
+      });
+        request.on('end', function()
+      {
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(error)
+        {
+          response.writeHead(302, {Location: `/`}); //한국어로 생성하면 오류 발생
+          response.end();
+        })
       });
     }
     else
